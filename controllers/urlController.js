d9c8df0cef879332761bb8e8e4369954ad7f50f7
PoojanBhalodiya <poojanbhalodiya02@gmail.com>
2024-01-31 23:46:51 +0530

    Issue solve

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,6 +1,7 @@
 const Url = require("../models/urlmodel");
 const shortid = require("shortid");
 const validUrl = require("valid-url");
+
 exports.redirectToOriginalUrl = async (req, res) => {
   const url = await Url.findOne({ shortUrl: req.params.shortUrl });
 
@@ -11,13 +12,14 @@ exports.redirectToOriginalUrl = async (req, res) => {
   }
 };
 
-exports.shortUrl = async (req, res) => {
+exports.shortenUrl = async (req, res) => {
   const { originalUrl } = req.body;
 
-  // validate the url
+  // Validate the URL
   if (!validUrl.isWebUri(originalUrl)) {
     return res.status(400).json({ error: "Invalid URL" });
   }
+
   const shortUrl = shortid.generate();
 
   // Create a new document in the database
